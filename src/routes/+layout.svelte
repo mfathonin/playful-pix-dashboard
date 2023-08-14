1ad05029d8cb86f8a0014d830aa0e7f7737bb0e2
Fathoni <mfathonin@gmail.com>
2023-08-14 18:46:36 +0700

    Change theme to skeleton default w/ modification

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import '../theme.postcss';
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	import '../app.postcss';
 	import { LightSwitch, Toast } from '@skeletonlabs/skeleton';
