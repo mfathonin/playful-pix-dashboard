b431e70f9949ee8e692560d8a0ba4a2280108979
Fathoni <mfathonin@gmail.com>
2023-08-14 10:26:09 +0700

    Trial to add Toast and LightSwitch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,13 @@
 	import '../theme.postcss';
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	import '../app.postcss';
+	import { LightSwitch, Toast } from '@skeletonlabs/skeleton';
 </script>
+
+<Toast position="tr" buttonDismiss="hidden" />
+
+<div class="fixed top-2 right-2">
+	<LightSwitch rounded="rounded-full" height="h-4" width="w-8" />
+</div>
 
 <slot />
