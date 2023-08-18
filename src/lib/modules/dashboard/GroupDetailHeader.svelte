2d2cdae950f6966bb39d7a34773592ae1824c173
Fathoni <mfathonin@gmail.com>
2023-08-18 20:48:04 +0700

    Finalize gruop detail header

--- src/lib/modules/dashboard/GroupDetailHeader.svelte
+++ src/lib/modules/dashboard/GroupDetailHeader.svelte
@@ -2,30 +2,31 @@
 	import type { Group } from '$lib/models/groups';
 
 	export let group: Group;
-	console.log(Object.entries(group.attributes || {}));
 </script>
 
 <div class="space-y-5">
 	<div class="space-y-1">
-		<h4 class="h4">{group.title}</h4>
+		<p class="text-secondary-500-400-token text-sm">Detail Buku</p>
+		<h4 class="h4 font-semibold">{group.title}</h4>
 	</div>
-	<div class="space-y-3 table-container">
-		{#if group.attributes}
-			<table class="table table-compact bg-transparent border-transparent">
+	{#if group.attributes}
+		<div class="table-container">
+			<table class="table bg-transparent border-transparent">
 				<tbody>
 					{#each Object.entries(group.attributes) as attr}
 						<tr class="table-row !border-transparent !bg-transparent">
-							<td class="table-cell !px-0 w-1/2 md:w-1/5 lg:w-2/12">
-								<div class="flex justify-between">
-									<span>{attr[0]}</span>
-									<span>:</span>
-								</div>
+							<td
+								class="table-cell !py-1 !px-0 w-1/2 md:w-1/5 lg:w-2/12 text-secondary-400-500-token"
+								>{attr[0]}</td
+							>
+							<td class="table-cell !py-1">
+								<span class="font-medium">{attr[1].join(', ')}</span>
 							</td>
-							<td>{attr[1].join(', ')}</td>
 						</tr>
 					{/each}
 				</tbody>
 			</table>
-		{/if}
-	</div>
+		</div>
+	{/if}
+	<hr class="!border-surface-200-700-token" />
 </div>
