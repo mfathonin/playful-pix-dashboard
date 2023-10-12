2a67b62309861dc034e0a836b5f51fe55411ee7f
Fathoni <mfathonin@gmail.com>
2023-10-12 12:33:39 +0700

    Fix on generating link and unauthorized access

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,9 +1,13 @@
+import { FirebaseAuth } from '$lib/services/firebase/firebase';
 import type { RequestEvent } from '@sveltejs/kit';
 
 export const authenticateUser = (event: RequestEvent) => {
 	const { cookies } = event;
+	// check using firebase auth
+	const isLoginFirebase = FirebaseAuth.currentUser !== null;
 
-	const isLogin = cookies.get('auth') !== undefined && cookies.get('auth') !== '';
+	const isLogin =
+		cookies.get('auth') !== undefined && cookies.get('auth') !== '' && isLoginFirebase;
 
 	return { isLogin };
 };
